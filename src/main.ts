42daec3ff56d0af297b1371dd4d348f2d9fa9204
ChanwooJeong0314 <101331450+ChanwooJeong0314@users.noreply.github.com>
2023-09-01 21:38:06 +0900

    Update main.ts
    이민수

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,11 @@ const app = express();
 const port : number = 3000;
 
 
-
+//router setting
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-
+//controllers
 app.use("/user",UserController);
 
-
+//listening port
 app.listen(port,()=> console.log(`listening on http://localhost:${port}`));
